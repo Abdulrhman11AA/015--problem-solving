8b6b17cfe39de1de32bebac7d75a981ef35982fd
Abdulrhman11AA <113008816+Abdulrhman11AA@users.noreply.github.com>
2022-10-26 01:28:49 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	char mat[5][5];
+	int counter=0,one=0,ii=0,jj=0;
+	for (int i = 0; i <5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> mat[i][j];
+			if (mat[i][j] == '1') ii = i, jj = j;
+			
+		}
+	}
+	bool x = true;
+	while (x) {
+		if (ii == 2 && jj == 2) {
+			cout << counter;
+			break;
+		}
+		if (ii > 2) {
+			while (ii != 2) {
+				ii--;
+				counter++;
+			}
+		}
+		if (ii < 2) {
+			while (ii!= 2) {
+				ii++;
+				counter++;
+			}
+		}
+		if (jj > 2) {
+			while (jj != 2) {
+				jj--;
+				counter++;
+			}
+		}
+		if (jj < 2) {
+			while (jj != 2) {
+				jj++;
+				counter++;
+			} 
+		}
+	}
+
+	return 0;
+}
